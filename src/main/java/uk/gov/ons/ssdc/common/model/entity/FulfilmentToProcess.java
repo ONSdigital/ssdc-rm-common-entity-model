8b242eec7dc90a657eb2749758d32ed2cf4fc46a
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-09-02 15:34:23 +0100

    Initial commit

---
+++ src/main/java/uk/gov/ons/ssdc/common/model/entity/FulfilmentToProcess.java
@@ -0,0 +1,24 @@
+package uk.gov.ons.ssdc.common.model.entity;
+
+import java.util.UUID;
+import javax.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class FulfilmentToProcess {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  @ManyToOne(optional = false)
+  private PrintTemplate printTemplate;
+
+  @ManyToOne(optional = false)
+  private Case caze;
+
+  @Column private Integer batchQuantity;
+
+  @Column private UUID batchId;
+}
